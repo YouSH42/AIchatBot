e2563b0aeeaaa80e6ba88ccf60d9abbcc26b2c8c
YouSH42 <enddl402@gmail.com>
2024-07-17 15:42:40 +0900

    chatBot

---
+++ main.py
@@ -0,0 +1,147 @@
+import os
+import streamlit as st
+import torch
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import ChatMessage
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.unstructured import UnstructuredFileLoader
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+
+# 필수 디렉토리 생성 @Mineru
+if not os.path.exists(".cache"):
+    os.mkdir(".cache")
+if not os.path.exists(".cache/embeddings"):
+    os.mkdir(".cache/embeddings")
+if not os.path.exists(".cache/files"):
+    os.mkdir(".cache/files")
+# =============================
+# .bashrc 파일에 
+# export HF_HOME="./.cache/" << 추가
+# =============================
+
+# 프롬프트를 자유롭게 수정해 보세요!
+RAG_PROMPT_TEMPLATE = """당신은 질문에 친절하게 답변하는 AI 입니다. 검색된 다음 문맥을 사용하여 질문에 답하세요. 답을 모른다면 모른다고 답변하세요.
+Question: {question} 
+Context: {context} 
+Answer:"""
+
+st.set_page_config(page_title="RAG를 이용한 챗봇", page_icon="💬")
+st.title("RAG를 이용한 챗봇")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        ChatMessage(role="assistant", content="무엇을 도와드릴까요?")
+    ]
+
+def print_history():
+    for msg in st.session_state.messages:
+        st.chat_message(msg.role).write(msg.content)
+
+def add_history(role, content):
+    st.session_state.messages.append(ChatMessage(role=role, content=content))
+
+def format_docs(docs):
+    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# 입력받은 문서를 임베딩하는 과정
+@st.cache_resource(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50,
+        separators=["\n\n", "\n", "(?<=\. )", " ", ""],
+        length_function=len,
+    )
+    # 다양한 문서를 파싱하기 위해서 unstructuredFileLoader를 사용
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=text_splitter)
+
+    # 모델 및 임베딩 설정
+    # gpu 가속을 위한 설정
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # 내가 따로 설정한 임베딩 모델
+    model_name = "bespin-global/klue-sroberta-base-continue-learning-by-mnr"
+    model_kwargs = {'device': device}
+    encode_kwargs = {'normalize_embeddings': True}
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+    )
+    # VectorDB로는 FAISS를 사용하여 구성하였음
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, embedding=cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def format_docs(docs):
+    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    return "\n\n".join(doc.page_content for doc in docs)
+
+with st.sidebar:
+    file = st.file_uploader(
+        "파일 업로드",
+        type=["pdf", "txt", "docx"],
+    )
+
+if file:
+    retriever = embed_file(file)
+
+print_history()
+
+if user_input := st.chat_input():
+    add_history("user", user_input)
+    st.chat_message("user").write(user_input)
+    with st.chat_message("assistant"):
+        # ngrok remote 주소 설정
+        ollama = ChatOllama(model="EEVE-Korean-10.8B:latest")
+        # ollama = RemoteRunnable(LANGSERVE_ENDPOINT)
+        chat_container = st.empty()
+        if file is not None:
+            prompt = ChatPromptTemplate.from_template(RAG_PROMPT_TEMPLATE)
+
+            # 체인을 생성합니다.
+            rag_chain = (
+                {
+                    "context": retriever | format_docs,
+                    "question": RunnablePassthrough(),
+                }
+                | prompt
+                | ollama
+                | StrOutputParser()
+            )
+            # 문서에 대한 질의를 입력하고, 답변을 출력합니다.
+            answer = rag_chain.stream(user_input)  # 문서에 대한 질의
+            chunks = []
+            for chunk in answer:
+                chunks.append(chunk)
+                chat_container.markdown("".join(chunks))
+            add_history("ai", "".join(chunks))
+        else:
+            prompt = ChatPromptTemplate.from_template(
+                "다음의 질문에 간결하게 답변해 주세요:\n{input}"
+            )
+
+            # 체인을 생성합니다.
+            chain = prompt | ollama | StrOutputParser()
+
+            answer = chain.stream(user_input)  # 문서에 대한 질의
+            chunks = []
+            for chunk in answer:
+                chunks.append(chunk)
+                chat_container.markdown("".join(chunks))
+            add_history("ai", "".join(chunks))
